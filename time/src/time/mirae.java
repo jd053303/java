18bb3d90f0c8018f4d2bc55ade4a172b2da797b6
jd053303 <64065652+jd053303@users.noreply.github.com>
2020-06-25 12:20:21 +0900

    Update mirae.java

--- time/src/time/mirae.java
+++ time/src/time/mirae.java
@@ -1,4 +1,4 @@
-/*6.13//6.17수정*/
+/*6.13//6.17수정//6.25글씨 위치 수정*/
 package time;
 
 import java.util.Scanner;
@@ -6,78 +6,80 @@ import java.util.Scanner;
 public class mirae {
 
 	public static void main(String[] args) {
-		Scanner sc  = new Scanner(System.in);
-		int month = mon();
-		int mon = month;
-		int day = da(mon);
-		int hour = hou();
-		
-		System.out.println("현재 : "+month+"월 "+day+"일 "); 
-		ttt(hour);
-		
-		System.out.println("몇 시간 후의 시간을 알려드릴까요? ");
-		int over = sc.nextInt();
-		int tt = hour + over ;
-		
-		if(tt>23) {
-			int a = tt/24;
-			day+=a;
-			tt-=(24*a);
-		}
-		if((month==1||month==3||month==5||month==7||month==8||month==10||month==12)&&day>31) { 
-			month += (day / 31);
-			day %= 31;
-		}
-		else if((month==4||month==6||month==9||month==11)&&day>30) { 
-			month += (day / 30);
-			day %= 30;
-		}
-		else if(month==2&&day>28) { 
-			month += (day / 28);
-			day %= 28;
-		}
-		if(month>12) { 
-			month %= 12;
-		}
-		
-		System.out.println(""+month+"월 "+day+"일");
-		ttt(tt);
-	}
-	
-	private static void ttt(int a) {
-		if(a>12) {
-			a=a-12;
-			System.out.print("오후"+ a);
-		}
-		else System.out.print("오전"+ a);
-	}
+		public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        int month = mon();
+        int mon = month;
+        int day = da(mon);
+        int hour = hou();
 
-	private static int hou() {
-		double rand = Math.random();
-		int hour = (int)(rand*23)+0;
-		
-		return hour;
-	}
+        System.out.printf("현재 : "+month+"월 "+day+"일 ");
+        ttt(hour);
 
-	private static int da(int a) {
-		double rand = Math.random();
-		if(a==1||a==3||a==5||a==7||a==8||a==10||a==12) {
-			 a = (int)(rand*31)+1;
-		}
-		else if(a==4||a==6||a==9||a==11 ) {
-			 a = (int)(rand*30)+1;
-		}
-		else {
-			 a = (int)(rand*28)+1;
-		}
-		
-		return a;
-	}
+        System.out.println("\n몇 시간 후의 시간을 알려드릴까요?");
+        System.out.printf("입력 : ");
+        int over = sc.nextInt();
+        int tt = hour + over ;
 
-	private static int mon() {
-		double rand = Math.random();
-		int month = (int)(rand*12)+1;
-		return month;
-	}
+        if(tt>23) {
+            int a = tt/24;
+            day+=a;
+            tt-=(24*a);
+        }
+        if((month==1||month==3||month==5||month==7||month==8||month==10||month==12)&&day>31) {
+            month += (day / 31);
+            day %= 31;
+        }
+        else if((month==4||month==6||month==9||month==11)&&day>30) {
+            month += (day / 30);
+            day %= 30;
+        }
+        else if(month==2&&day>28) {
+            month += (day / 28);
+            day %= 28;
+        }
+        if(month>12) {
+            month %= 12;
+        }
+        System.out.printf("\n"+over+"시 후 : ");
+        System.out.printf(""+month+"월 "+day+"일 ");
+        ttt(tt);
+    }
+
+    private static void ttt(int a) {
+        if(a>12) {
+            a=a-12;
+            System.out.println("오후"+ a);
+        }
+        else System.out.println("오전"+ a);
+    }
+
+    private static int hou() {
+        double rand = Math.random();
+        int hour = (int)(rand*23)+0;
+
+        return hour;
+    }
+
+    private static int da(int a) {
+        double rand = Math.random();
+        if(a==1||a==3||a==5||a==7||a==8||a==10||a==12) {
+            a = (int)(rand*31)+1;
+        }
+        else if(a==4||a==6||a==9||a==11 ) {
+            a = (int)(rand*30)+1;
+        }
+        else {
+            a = (int)(rand*28)+1;
+        }
+
+        return a;
+    }
+
+    private static int mon() {
+        double rand = Math.random();
+        int month = (int)(rand*12)+1;
+        return month;
+    }
 
 }
