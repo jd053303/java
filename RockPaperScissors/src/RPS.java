de5a69bbe84efb614a1b71d9c65663c5e4ce68a7
jd053303 <64065652+jd053303@users.noreply.github.com>
2020-07-10 12:00:48 +0900

    Update RPS.java

--- RockPaperScissors/src/RPS.java
+++ RockPaperScissors/src/RPS.java
@@ -1,36 +1,49 @@
-/*6.18*/
+/* 6.18 / 7.10 */ 
 import java.util.Scanner;
 
 public class RPS {
 	static Scanner scan = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		double rand = Math.random();
-		int pc = (int)(rand*3)+1;
-		System.out.println("!Rock-paper-Scissors game with PC!");
-		System.out.println("1.Scissors  2.Rock  3.Paper");
-		System.out.println("\rME : ");
-		int me = scan.nextInt();
-		int a = pc + me;
-		
-		System.out.print("\rPC : ");
-		han(pc);
-		System.out.print("ME : ");
-		han(me);
-		
-		if(pc==me) System.out.println("\r\r!DRAW!");
-		else if(a==4) {
-			if(me<pc) System.out.println("\r\r!WIN!");
-			else System.out.println("\r\r!LOSE!");
+		int l=0;
+		do {
+			if( l==1 ) clearScreen();
+			double rand = Math.random();
+			int pc = (int) (rand * 3) + 1;
+			System.out.println("!Rock-paper-Scissors game with PC!");
+			System.out.println("1.Scissors  2.Rock  3.Paper");
+			System.out.print("\rME : ");
+			int me = scan.nextInt();
+			int a = pc + me;
+
+			System.out.print("\nPC : ");
+			han(pc);
+			System.out.print("ME : ");
+			han(me);
+
+			if (pc == me) System.out.println("   !DRAW!");
+			else if (a == 4) {
+				if (me < pc) System.out.println("   !WIN!");
+				else System.out.println("   !LOSE!");
+			} else {
+				if (me > pc) System.out.println("   !WIN!");
+				else System.out.println("   !LOSE!");
 			}
-		else {
-			if(me>pc) System.out.println("\r\r!WIN!");
-			else System.out.println("\r\r!LOSE!");
-		}
+
+			System.out.println("\n다시 하고 싶으면 1을 누르세요.");
+			l = scan.nextInt();
+		}while(l==1);
 	}
-	
-	private static void han (int h) {
-		if(h==1) System.out.println("Scissors");
-		else if(h==2) System.out.println("Rock");
-		else System.out.println("Paper");	
+
+	private static void han ( int h){
+		if (h == 1) System.out.println("Scissors");
+		else if (h == 2) System.out.println("Rock");
+		else System.out.println("Paper");
+	}
+
+	public static void clearScreen() {
+		for (int i = 0; i < 80; i++)
+			System.out.println("");
 	}
 }
+
